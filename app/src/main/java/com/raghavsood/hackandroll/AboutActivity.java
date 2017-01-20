15ab23584b4b41c3cbedb9ae05cfe85304ea9b78
Raghav Sood <raghavsood@appaholics.in>
2017-01-21 01:12:45 +0800

    LinearLayout and going back

--- app/src/main/java/com/raghavsood/hackandroll/AboutActivity.java
+++ app/src/main/java/com/raghavsood/hackandroll/AboutActivity.java
@@ -1,7 +1,9 @@
 package com.raghavsood.hackandroll;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
