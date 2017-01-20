ebd657b882875121792db1c1379dc5f2d8e7582a
Raghav Sood <raghavsood@appaholics.in>
2017-01-21 00:52:35 +0800

    Performing actions on Button clicks

--- app/src/main/java/com/raghavsood/hackandroll/MainActivity.java
+++ app/src/main/java/com/raghavsood/hackandroll/MainActivity.java
@@ -3,13 +3,26 @@ package com.raghavsood.hackandroll;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ImageInterface {
+
+    Button surpriseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surpriseButton = (Button) findViewById(R.id.btn_download);
+        surpriseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
