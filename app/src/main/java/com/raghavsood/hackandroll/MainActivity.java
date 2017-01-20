dfd018d4136ef753e36ce7a45089230f145abf52
Raghav Sood <raghavsood@appaholics.in>
2017-01-21 01:10:31 +0800

    Launching an Activity

--- app/src/main/java/com/raghavsood/hackandroll/MainActivity.java
+++ app/src/main/java/com/raghavsood/hackandroll/MainActivity.java
@@ -1,8 +1,11 @@
 package com.raghavsood.hackandroll;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -47,5 +50,25 @@ public class MainActivity extends AppCompatActivity implements ImageInterface {
                 imageView.setImageBitmap(bitmaps[i++]); // Display the next image
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_about) {
+            // Create an Intent to go to the About activity
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent); // Launch the About activity
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
