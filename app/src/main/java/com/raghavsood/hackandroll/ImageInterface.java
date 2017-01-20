2a33284d11de4f3a40dc0713b6e3a3b452d98ec9
Raghav Sood <raghavsood@appaholics.in>
2017-01-21 00:45:28 +0800

    Creating the ImageInterface class

---
+++ app/src/main/java/com/raghavsood/hackandroll/ImageInterface.java
@@ -0,0 +1,8 @@
+package com.raghavsood.hackandroll;
+
+import android.graphics.Bitmap;
+
+public interface ImageInterface {
+    public void updateImages(Bitmap bitmaps[]);
+}
+
--- app/src/main/java/com/raghavsood/hackandroll/MainActivity.java
+++ app/src/main/java/com/raghavsood/hackandroll/MainActivity.java
@@ -1,13 +1,19 @@
 package com.raghavsood.hackandroll;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ImageInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void updateImages(Bitmap[] bitmaps) {
+        //TODO Create and update an ImageView
     }
 }
